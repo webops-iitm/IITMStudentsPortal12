83d2dd156f3f3fb536cc9a9b1a71c7ad462f4259
Vineet Upadhyay <vineet.1991.483@gmail.com>
2012-09-29 13:24:42 +0530

    added messlist

---
+++ apps/home/messlist.php
@@ -0,0 +1,50 @@
+<div class="widget-header">
+		<i class="icon-comment"></i>
+		<h3>Mess Registrations</h3>
+</div> <!-- /widget-header -->
+		<div class="widget-content">						
+			<div class="shortcuts">
+	<center>
+            <strong>Caterer Wise Mess Allocation List</strong><br>
+	<table>
+           <tr>
+		<td class="span2"><a href="/files/Mess/FC.pdf" >FoodCourt</a></td>
+              <td class="span2"><a href="/files/Mess/Himalaya RR.pdf" >Himalaya - RR</a></td>
+              <td class="span2"><a href="/files/Mess/Himalaya SK.pdf" >Himalaya - SK</a></td>
+              <td class="span2"><a href="/files/Mess/HM Cauvery.pdf" >HM Cauvery</a></td>
+            </tr><br><tr>
+		<td><a href="/files/Mess/HM Krishna.pdf" >HM Krishna</a></td>
+              <td><a href="/files/Mess/ISS.pdf" >ISS Mess</a></td>
+              <td><a href="/files/Mess/Vindhya M1.pdf" >Vindhya M1 </a></td>
+              <td><a href="/files/Mess/Vindhya M2.pdf" >Vindhya M2 </a></td></tr>
+	</table><br><br>
+            <strong>Hostel Wise Mess Allocation List</strong><br>
+        <table>
+	<tr> 
+		<td class="span2">  <a href="/files/Mess/Alaknanda.pdf" >Alaknanda</a><br></td>
+              <td class="span2">  <a href="/files/Mess/Brahmaputra.pdf" >Brahmaputra</a><br></td>
+              <td class="span2">  <a href="/files/Mess/Cauvery.pdf" >Cauvery</a><br></td>
+              <td class="span2">  <a href="/files/Mess/Ganga.pdf" >Ganga</a><br></td>
+	</tr><tr>
+              <td>  <a href="/files/Mess/Godavari.pdf" >Godavari</a><br></td>
+              <td>  <a href="/files/Mess/Jamuna.pdf" >Jamuna</a><br></td>
+              <td>  <a href="/files/Mess/Krishna.pdf" >Krishna</a><br></td>
+              <td>  <a href="/files/Mess/Mahanadi.pdf" >Mahanadi</a><br></td>
+	</tr><tr>
+              <td>  <a href="/files/Mess/Mandakini.pdf" >Mandakini</a><br></td>
+              <td>  <a href="/files/Mess/Narmada.pdf" >Narmada</a><br></td>
+              <td>  <a href="/files/Mess/Pampa.pdf" >Pampa</a><br></td>
+              <td>  <a href="/files/Mess/Saraswati.pdf" >Saraswathi</a><br></td>
+	</tr><tr>
+              <td>  <a href="/files/Mess/Sarayu Ext.pdf" >Sarayu Ext</a><br></td>
+              <td>  <a href="/files/Mess/Sarayu.pdf" >Sarayu</a><br></td>
+              <td>  <a href="/files/Mess/Sharavati.pdf" >Sharavathi</a><br></td>
+              <td>  <a href="/files/Mess/Sindhu.pdf" >Sindhu</a><br></td>
+	</tr><tr>
+              <td>  <a href="/files/Mess/Tamiraparani.pdf" >Tamiraparani</a><br></td>
+              <td>  <a href="/files/Mess/Tapti.pdf" >Tapti</a><br></td>              
+	</tr>
+</table>	
+</center>
+		</div>	
+</div>

1a6c1a858450042abf08828fc0c80b542260a83a
ravikanthuppuluri <ravikanthuppuluri@gmail.com>
2012-10-06 01:51:34 +0530

    Hosting code for student search

--- apps/students-search/left-content.php
+++ apps/students-search/left-content.php
@@ -1,12 +1,339 @@
+
+
+
+
 <div class="span8">
 	<div class="widget" style="width:800px; margin:10px; position:relative; top:0px; right:0px;">
 		<div class="widget-header">
-			<i class="icon-comment"></i>
-			<h3>News</h3>
+			<i class="icon-search"></i>
+			<h3>Student search</h3>
 		</div> <!-- /widget-header -->
 		<div class="widget-content">						
-			<div class="shortcuts">	
-ravi ipsum dolor sit amet Consectetur adipiscing elit Integer molestie lorem at massa Facilisis in pretium nisl aliquet Nulla volutpat aliquam velit Phasellus iaculis neque Purus sodales ultricies Vestibulum laoreet porttitor sem Ac tristique libero volutpat at Faucibus porta lacus fringilla vel Aenean sit amet erat nunc Eget porttitor lorem.
+			<div class="shortcuts">
+Enter 'Name' or 'Nick' or 'Rollno' or 'Room' or 'Hostel'
+<br/>	<br/>  
+<form name="form1" action="student-search.php" method="get">
+  <table><tr><td><input type="text"  name="q" /></td><td><input style="position:relative;top:-4px;"class="btn btn-primary" type="submit" name="Submit" value="Search" /></td></tr>
+  
+  <tr><td></td><td><a class="showadv" href="#"><i class="advic icon-plus-sign"></i> Advanced Search</a><td></tr></table>
+</form>
+
+
+
+
+
+
+
+
+<form  style="display:none;" class="adv" name="form2" action="student-search.php" method="get">
+<table style="border-top:1px solid black ;">
+<tr><td>&nbsp;</td><td>&nbsp;</td></tr>
+<tr><td>&nbsp;</td><td>Atleast one search hint is necessary</td></tr>
+<td></tr><tr><td>&nbsp;</td><td>&nbsp;</td>
+<tr>
+<td>
+Name
+</td>
+
+<td>
+<input type="text" name="a" />
+</td>
+</tr>
+
+<tr>
+<td>
+Nick
+</td>
+
+<td>
+<input type="text" name="b" />
+</td>
+</tr>
+
+<tr>
+<td>
+Roll
+</td>
+<td>
+<input type="text" name="c" />
+</td>
+</tr>
+
+
+<tr>
+<td>
+Hostel
+</td>
+
+<td>
+<select name="d">
+<option value="" selected="selected" >Select</option>
+<option value="Alakananda">Alakananda</option>
+<option value="Brahmaputra">Brahmaputra</option>
+<option value="Cauvery">Cauvery</option>
+<option value="Ganga">Ganga</option>
+<option value="Godavari">Godavari</option>
+<option value="Jamuna">Jamuna</option>
+<option value="Krishna">Krishna</option>
+<option value="Mahanadhi">Mahanadhi</option>
+<option value="Mandakini">Mandakini</option>
+<option value="Narmada">Narmada</option>
+<option value="Saraswathi">Saraswathi</option>
+<option value="Sarayu">Sarayu</option>
+<option value="Sharavati">Sharavati</option>
+<option value="Sindhu">Sindhu</option>
+<option value="Tapti">Tapti</option>
+
+
+
+
+
+
+
+</select>
+</td>
+</tr>
+
+<tr>
+<td>
+Room No: 
+</td>
+
+<td>
+<input type="text" name="e" />
+</td>
+</tr>
+
+
+
+<tr>
+<td>
+&nbsp;
+</td>
+<td>
+<input class="btn btn-primary" type="submit" name="Submit" value="Search" />
+</td>
+</tr>
+
+</table>
+
+<input type="hidden" name="searching" value="yes" />
+</form>
+
+<script src="apps/students-search/jquery.js"></script>
+<script> 
+$(document).ready(function(){
+
+   var a=0;
+
+$(".showadv").click(function(){
+
+if(a==0)
+{
+
+  $(".adv").css("display","inline");
+  $(".advic").removeClass("icon-plus-sign");
+  $(".advic").addClass("icon-minus-sign");
+  a=1;
+} 
+ else
+{
+   $(".adv").css("display","none");
+   $(".advic").removeClass("icon-minus-sign");
+   $(".advic").addClass("icon-plus-sign");
+   a=0;
+}
+ 
+
+}); 
+ 
+});
+</script>
+
+
+
+
+<?php
+
+// if (isset($_SESSION['uname']))
+
+//  {
+ if (isset($_GET['q']))
+  {
+
+
+  // Get the search variable from URL
+
+  $var = @$_GET['q'] ;
+  
+  
+  $var = strtoupper($var); //converting to uppercase
+  $trimmed = trim($var) ; //trim whitespace from the stored variable
+  
+  
+  
+
+
+  
+  
+  
+  
+  // check for an empty string and display a message.
+if ($trimmed == "")
+  {
+  echo "<p style='color:red;'>Please enter a search...</p>";
+  exit;
+  }
+  
+  // Include database connection settings
+include('config.inc');
+
+
+ $query = "select * from users where upper(fullname) like \"%$trimmed%\" OR upper(nick) like \"%$trimmed%\" OR upper(username) like \"%$trimmed%\" OR upper(hostel) like \"%$trimmed%\" OR upper(room) like \"%$trimmed%\" 
+ 
+  order by fullname"; 
+  $numresults=mysql_query($query);
+ $numrows=mysql_num_rows($numresults);
+ echo "<h3>Results</h3>";
+ if ($numrows == 0)
+  {
+  
+  echo "<p style='color:red;'>Sorry, your search: &quot;" . $trimmed . "&quot; returned zero results</p>";
+  exit;
+  }
+  
+  echo "<table border='1' style='background:white;border:10px solid #E9E9E9 ;' align='center' >
+<tr>
+<th>Name</th>
+<th>Nick</th>
+<th>Roll No</th>
+<th>Hostel</th>
+<th>Room No</th>
+</tr>";
+
+while($row = mysql_fetch_array($numresults))
+  {
+  echo "<tr>";
+  echo "<td>" . $row['fullname'] . "</td>";
+  echo "<td>" . $row['nick'] . "</td>";
+  echo "<td>" . $row['username'] . "</td>";
+  echo "<td>" . $row['hostel'] . "</td>";
+  echo "<td>" . $row['room'] . "</td>";
+  echo "</tr>";
+  }
+echo "</table>";
+
+mysql_close($con);
+
+
+
+}
+
+
+
+
+
+
+
+ if (isset($_GET['searching']))
+  {
+
+
+  // Get the search variable from URL
+
+  $var1 = @$_GET['a'] ;
+  $var2 = @$_GET['b'] ;
+  $var3 = @$_GET['c'] ;
+  $var4 = @$_GET['d'] ;
+  $var5 = @$_GET['e'] ;
+  
+  
+  $var1 = strtoupper($var1); //converting to uppercase
+  $trimmed1 = trim($var1) ; //trim whitespace from the stored variable
+  
+  
+  $var2 = strtoupper($var2); //converting to uppercase
+  $trimmed2 = trim($var2) ; //trim whitespace from the stored variable
+  
+  $var3 = strtoupper($var3); //converting to uppercase
+  $trimmed3 = trim($var3) ; //trim whitespace from the stored variable
+  
+  
+  $var4 = strtoupper($var4); //converting to uppercase
+  $trimmed4 = trim($var4) ; //trim whitespace from the stored variable
+  
+  $var5 = strtoupper($var5); //converting to uppercase
+  $trimmed5 = trim($var5) ; //trim whitespace from the stored variable
+  
+  
+  
+  
+
+
+  
+  
+  
+  
+  // check for an empty string and display a message.
+if ($trimmed1 == "" && $trimmed2 == "" && $trimmed3 == "" && $trimmed4 == "" && $trimmed5 == "")
+  {
+  echo "<p style='color:red;'>Please enter atleast one search hint...</p>";
+  exit;
+  }
+  
+  // Include database connection settings
+include('config.inc');
+
+
+ $query = "select * from users where upper(fullname) like \"%$trimmed1%\" AND upper(nick) like \"%$trimmed2%\" AND upper(username) like \"%$trimmed3%\" AND upper(hostel) like \"%$trimmed4%\" AND upper(room) like \"%$trimmed5%\" 
+ 
+  order by fullname"; 
+  $numresults=mysql_query($query);
+ $numrows=mysql_num_rows($numresults);
+ 
+ 
+ echo "<h3>Results</h3>";
+ 
+ if ($numrows == 0)
+  {
+  
+  echo "<p style='color:red;'>Sorry, your search returned zero results</p>";
+  exit;
+  }
+  
+  echo "<table border='1' style='background:white;border:10px solid #E9E9E9 ;' align='center'>
+<tr>
+<th>Name</th>
+<th>Nick</th>
+<th>Roll No</th>
+<th>Hostel</th>
+<th>Room No</th>
+</tr>";
+
+while($row = mysql_fetch_array($numresults))
+  {
+  echo "<tr>";
+  echo "<td>" . $row['fullname'] . "</td>";
+  echo "<td>" . $row['nick'] . "</td>";
+  echo "<td>" . $row['username'] . "</td>";
+  echo "<td>" . $row['hostel'] . "</td>";
+  echo "<td>" . $row['room'] . "</td>";
+  echo "</tr>";
+  }
+echo "</table>";
+
+mysql_close($con);
+
+
+
+}
+
+//  }
+//   else {echo "Only for logged in users" ;}
+
+
+?>
+<br/><br/>
 			</div>	
 		</div>
 	</div>

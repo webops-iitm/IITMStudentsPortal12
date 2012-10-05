20f1b4f89a6c408b1f53c368ebac3b19c89b65b0
ravikanthuppuluri <ravikanthuppuluri@gmail.com>
2012-10-05 21:48:05 +0530

    Learning Git

--- apps/students-search/left-content.php
+++ apps/students-search/left-content.php
@@ -6,7 +6,7 @@
 		</div> <!-- /widget-header -->
 		<div class="widget-content">						
 			<div class="shortcuts">	
-Lorem ipsum dolor sit amet Consectetur adipiscing elit Integer molestie lorem at massa Facilisis in pretium nisl aliquet Nulla volutpat aliquam velit Phasellus iaculis neque Purus sodales ultricies Vestibulum laoreet porttitor sem Ac tristique libero volutpat at Faucibus porta lacus fringilla vel Aenean sit amet erat nunc Eget porttitor lorem.
+ravi ipsum dolor sit amet Consectetur adipiscing elit Integer molestie lorem at massa Facilisis in pretium nisl aliquet Nulla volutpat aliquam velit Phasellus iaculis neque Purus sodales ultricies Vestibulum laoreet porttitor sem Ac tristique libero volutpat at Faucibus porta lacus fringilla vel Aenean sit amet erat nunc Eget porttitor lorem.
 			</div>	
 		</div>
 	</div>

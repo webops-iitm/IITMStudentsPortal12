238a03d324ecebdc918ecd7ac11628b03daebc17
ravikanthuppuluri <ravikanthuppuluri@gmail.com>
2012-10-07 00:32:47 +0530

    Fixing error with login box of student search page

--- student-search.php
+++ student-search.php
@@ -1,5 +1,13 @@
 <?php
 	include("db.php");
+
+         session_start();
+	
+	if (isset($_COOKIE["user"]))
+		$_SESSION['uname'] = $_COOKIE["user"];
+
+	include("config.php");
+
 	include("header.php");
 
     

a432f55ee7572c655b2b3754a4b94e2738a66275
vsvrp-1995 <vsvrp.1995@gmail.com>
2012-10-29 19:06:19 +0530

    SAC meetings updated

--- apps/home/sac-meeting.php
+++ apps/home/sac-meeting.php
@@ -1,10 +1,13 @@
-<div class="widget-header">
+<div class="widget-header" id="accordion2">
+<div class="accordion-group">
+<div class="accordion-heading"><a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapsemainone">
 		<i class="icon-star"></i>
-		<h3>Minutes of SAC Meeting Held on 30.01.2012 at MRC</h3>
+		<h3>Minutes of SAC Meeting Held on 30.01.2012 at MRC</h3></a></div>
+</div>
 </div> <!-- /widget-header -->
 
 <div class="widget-content">
-
+<div id="collapsemainone" class="collapse"><div class="accordion-inner">
 ATTENDED BY: &nbsp;
 SSAC, SGS, HAS, CAS (A), CAS (L), CCAS, AAS<br><br>
 SAC MEMBERS PRESENT: &nbsp;
@@ -47,7 +50,7 @@ MINUTES:<br>
 						</div>
 						<div id="collapseThree" class="accordion-body collapse">
 								<div class="accordion-inner">
-										An appeal was made to relax the time restriction for girls to be present in boys� hostel during hostel nights from the 9pm to 12am. SAC passed this appeal and will forward the appeal to concerned authorities.
+										An appeal was made to relax the time restriction for girls to be present in boys hostel during hostel nights from the 9pm to 12am. SAC passed this appeal and will forward the appeal to concerned authorities.
 								</div>
 						</div>
 				</div><!--accordion-group-->
@@ -59,7 +62,7 @@ MINUTES:<br>
 						</div>
 						<div id="collapseFour" class="accordion-body collapse">
 								<div class="accordion-inner">
-										Request for a holiday on the occasion of Holiday (8th March�12) was made and SAC passed the motion. The request will now be made on behalf of students to the concerned authorities.
+										Request for a holiday on the occasion of Holiday (8th March12) was made and SAC passed the motion. The request will now be made on behalf of students to the concerned authorities.
 								</div>
 						</div>
 				</div><!--accordion-group-->
@@ -72,6 +75,233 @@ MINUTES:<br>
 						<div id="collapseFive" class="accordion-body collapse">
 								<div class="accordion-inner">
 										CCAS. CAS (A) and CAS (L) presented 5 possible scenarios for scheduling Shaastra and Saarang. With no major objections and recommendations from the SAC, they will be presenting the scenarios on behalf of the student community to the concerned authorities.
+								</div>
+						</div>
+				</div><!--accordion-group-->
+		</div>
+</div>
+</div>
+</div>
+<div class="widget-header" id="accordion2">
+<div class="accordion-group">
+<div class="accordion-heading"><a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapsemaintwo">
+		<i class="icon-star"></i>
+		<h3>Minutes of SAC Meeting held on 18th September 2012:</h3></a>
+</div> <!-- /widget-header -->
+</div>
+</div>
+<div class="widget-content">
+<div id="collapsemaintwo" class="collapse"><div class="accordion-inner">
+The Dean (Students) spoke about the importance of redressal mechanisms in the institute.<br>
+The need for students to clarify their issues via proper channels was emphasized upon.<br>
+He also briefed about the processes involved in the selection of messes for the past academic year and the present year. <br><br>
+A representative from MITr will be made a special invitee to SAC meetings.<br><br>
+Prakash Reddy Putta has been selected as the 'Institute Film Club Student Head'.<br><br>
+A Female Security committee has been formed comprising the Speaker SAC, SGS, RAS, the general secretaries of Sharavati and Sarayu and volunteers.<br><br>
+		
+		<div class="accordion" id="accordion2">
+				<div class="accordion-group">
+						<div class="accordion-heading">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseSix">
+										Regarding Students Facilities
+								</a>
+						</div>
+						<div id="collapseSix" class="accordion-body collapse">
+								<div class="accordion-inner"><ul>
+<li>All India Travel Agency outlet at Gurunath shall be removed and replaced with a Railway Tatkal Counter</li>
+<li>seating capacity at SFC will be increased , Engineering Unit has already initiated the works.</li>
+<li>Engineering Unit has initiated repairs works for Road & Parking lights at Himalaya.</li>
+<li>New items will be added in GFC and also negotiation of prices will be done in the next two weeks</li>
+<li>Barbecue section will be opened in Zaitoon</li>
+<li>Hostel Delivery will start from Zaitoon & Gurunath soon.</li>
+<li>A new Complex is planned near the girl's hostels with facilities for stationery/ General Items, cycle repair, Beauty Parlour, 24 Hour Canteen etc.</li>
+<li>a 24 hour canteen near office of hostel management will be opened within the next one and a half month.</li>
+<li>24 Hour Internet facility during examination period.</li>
+<li>Free Photocopy facility for students , IIT Madras</li></ul>
+								</div>
+						</div>
+				</div><!--accordion-group-->
+				<div class="accordion-group">
+						<div class="accordion-heading">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseSeven">
+										Power Laundry
+								</a>
+						</div>
+						<div id="collapseSeven" class="accordion-body collapse">
+								<div class="accordion-inner"><ul>
+<li>Work of a power laundry service will be started by 27th September,2012 (Tentatively).</li>
+<li>Laundry operations will start at Brahmaputra Hostel basement after rainy season. Till then, It will be operated from outside, while distribution & ironing would be operated from brahmaputra basement.</li></ul>
+								</div>
+						</div>
+				</div><!--accordion-group-->
+				<div class="accordion-group">
+						<div class="accordion-heading">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseEight">
+										 Cycle Sharing System is being looked into where in
+								</a>
+						</div>
+						<div id="collapseEight" class="accordion-body collapse">
+								<div class="accordion-inner"><ul><li>Cycle would be maintained by outside vendor.</li>
+<li>Students can borrow cycles from different points of the institute.</li>
+<li>Charges will be based on the duration for which cycle is borrowed and the type of cycle borrowed</li></ul>
+								</div>
+						</div>
+				</div><!--accordion-group-->
+				<div class="accordion-group">
+						<div class="accordion-heading">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseNine">
+										Disaster Management Committee:
+								</a>
+						</div>
+						<div id="collapseNine" class="accordion-body collapse">
+								<div class="accordion-inner"><ul>
+<li>A committee has been formulated.</li>
+<li>Conducting workshops on fire drills & Emergency medical care till semester. </li>
+<li>Analyzing disaster management plan for bigger venues like OAT, SAC etc</li></ul>				</div>
+						</div>
+				</div><!--accordion-group-->
+				<div class="accordion-group">
+						<div class="accordion-heading">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseTen">
+ Institute Web-ops and Mob-Ops team has been formed. It shall help in:
+								</a>
+						</div>
+						<div id="collapseTen" class="accordion-body collapse">
+								<div class="accordion-inner">
+<ul><li>A better webpage design for the institute website</li>
+<li>Institute mob-ops team for mobile application for handy and easy access to necessary information of the institute</li></ul>
+								</div>
+						</div>
+				</div><!--accordion-group-->
+				
+				<div class="accordion-group">
+						<div class="accordion-heading">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseEleven">
+ Mess
+								</a>
+						</div>
+						<div id="collapseEleven" class="accordion-body collapse">
+								<div class="accordion-inner"><ul>
+<li>An external party has been appointed to monitor the mess</li>
+<li>New items requested in the food court will be added soon.</li>
+<li>The following changes are being proposed in mess (Approved by SAC Members)</li>
+<li>Proposal for Bio metric fingerprint check to enter the mess</li>
+<li>Using Student ID card for extras and</li>
+<li>Online mess account</li></ul>			</div>
+						</div>
+				</div><!--accordion-group-->
+				
+				<div class="accordion-group">
+						<div class="accordion-heading">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwelve">
+Regarding Slow Internet:
+								</a>
+						</div>
+						<div id="collapseTwelve" class="accordion-body collapse">
+								<div class="accordion-inner">Prof. Nitin Chandrachoodan addressed the students. He stated that<ul>
+<li>Problem was not due to bandwidth limitation</li>
+<li>Also Wi-Fi replacements are planned in current Wi-Fi locations</li>
+<li>A committee has been formulated to address students’ issues with regard to Hostel Management Staff</li>
+<li>Procedure to use Hostel student welfare fund was explained by HAS & SGS.</li>
+								</div>
+						</div>
+				</div><!--accordion-group-->
+				<b>Update by Research Affairs Secretary:</b>
+				<div class="accordion-group">
+						<div class="accordion-heading">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseThirteen">
+Re-News:
+								</a>
+						</div>
+						<div id="collapseThirteen" class="accordion-body collapse">
+								<div class="accordion-inner">
+A monthly research newsletter was brought out, to enable the passage of information/news among and between the research scholars. It is being uploaded in the official website of T5E, <a href="http://students.iitm.ac.in/thefifthestate/links/renews/">http://students.iitm.ac.in/thefifthestate/links/renews/</a>, under the independent publications.
+								</div>
+						</div>
+				</div><!--accordion-group-->
+				<div class="accordion-group">
+						<div class="accordion-heading">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseFourteen">
+IITM Alumni Mentor Pilot Project: 					</a>
+						</div>
+						<div id="collapseFourteen" class="accordion-body collapse">
+								<div class="accordion-inner">A pilot project was initiated in coordination with IITMAA where students having difficulties in academics or in choosing career options are put in contact with IITM Alumni, who help them in due course. Huge response with 15 requests in 3 hrs and registration had to be closed in 3 hrs. As it was first come first served and we were planning to take only 4 students for the project. It will be restarted once the official website is launched.
+								</div>
+						</div>
+				</div><!--accordion-group-->
+				<div class="accordion-group">
+						<div class="accordion-heading">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseFifteen">
+Lectures:
+								</a>
+						</div>
+						<div id="collapseFifteen" class="accordion-body collapse">
+								<div class="accordion-inner">
+Monthly Technical Lecture Series has been started wherein experts from various fields are invited to give lectures on various topics.
+Joy of Science Lecture Series will also be organized.
+								</div>
+						</div>
+				</div><!--accordion-group-->
+				<div class="accordion-group">
+						<div class="accordion-heading">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseSixteen">
+Research Guidance Cell:
+								</a>
+						</div>
+						<div id="collapseSixteen" class="accordion-body collapse">
+								<div class="accordion-inner">A Cell has been created to address the complaints of scholars.
+								</div>
+						</div>
+				</div><!--accordion-group-->
+				<div class="accordion-group">
+						<div class="accordion-heading">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseSeventeen">
+Workshops:
+								</a>
+						</div>
+						<div id="collapseSeventeen" class="accordion-body collapse">
+								<div class="accordion-inner">
+Springer Author’s workshop has been organized.
+John Wiley’s Author Workshop will be organized.
+								</div>
+						</div>
+				</div><!--accordion-group-->
+				<div class="accordion-group">
+						<div class="accordion-heading">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseEighteen">
+Reg. Cultural and Co-Curricular Activities:
+								</a>
+						</div>
+						<div id="collapseEighteen" class="accordion-body collapse">
+								<div class="accordion-inner"><ul>
+<li>There will be an Increment in the number of flagship events at the fest</li>
+<li>Dream Theater Keyboardist will be performing at the OAT on 21st of October, 2012</li>
+<li>Adobe workshop in October for Photoshop and other software.</li>
+								</ul></div>
+						</div>
+				</div><!--accordion-group-->
+				<div class="accordion-group">
+						<div class="accordion-heading">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseNinteen">
+Reg. Sports:
+								</a>
+						</div>
+						<div id="collapseNinteen" class="accordion-body collapse">
+								<div class="accordion-inner">
+New contract has been awarded for swimming pool. It is expected to  be functional by the beginning of next academic year.
+								</div>
+						</div>
+				</div><!--accordion-group-->
+				<div class="accordion-group">
+						<div class="accordion-heading">
+								<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwenty">
+Reg. Academics:
+								</a>
+						</div>
+						<div id="collapseTwenty" class="accordion-body collapse">
+								<div class="accordion-inner">
+A request to provide attendance for students who will be taking leave in various activities representing the institute or conferences will be forwarded to board of academic courses.<br>
+Early registration for 1st year students for the next semester will be decided after taking the view of the students.
 								</div>
 						</div>
 				</div><!--accordion-group-->

22f8992b416d0490068a07f9dcb10da3bfc8920c
prasanthsai <g.prasanthsai@gmail.com>
2012-11-01 09:14:58 -0700

    Share a cab

---
+++ apps/share-a-cab/function.php
@@ -0,0 +1,56 @@
+<?php
+
+class ShareACab
+{
+	
+	public function __construct() 
+		{
+			include"../../db.php";
+		}
+	
+	
+	public function get_session() 
+		{
+			return $_SESSION['uname'];
+		}
+	function add_new($bookedby, $origin, $destination, $date, $time, $numseats, $numvacancy, $cabtype, $comment)
+		{
+			$query = "INSERT INTO share_a_cab (bookedby, origin, destination, date, time, numseats, numvacancy, cabtype, comment) VALUES ('$bookedby', '$origin', '$destination', '$date', '$time', '$numseats', '$numvacancy', '$cabtype', '$comment')";
+			$result= mysql_query($query) or die('Couldnt process');
+			return $result;
+		}
+	function validate_new()
+		{
+			if(!empty($_GET['origin']))
+				{
+					if(!empty($_GET['destination'])){}
+				}
+		}
+	function search_cab($origin, $destination, $date, $numrequired, $cabtype)
+		{
+			$query = "SELECT * FROM share_a_cab WHERE origin = '$origin' AND destination = '$destination' AND date = '$date' ORDER BY time DESC";
+			$result = mysql_query($query) or die("Couldnt process your request");
+			return $result;
+		}
+		
+	function scheduled()
+		{
+			$query = "SELECT * FROM share_a_cab ORDER BY date DESC";
+			$result = mysql_query($query) or die("Couldnt process your request");
+			return $result;
+		}
+	function select_cab($rid)
+		{
+			$query = "SELECT * FROM share_a_cab WHERE rid = '$rid'";
+			$result = mysql_query($query) or die("Couldnt process your request");
+			return $result;
+		}
+	function add_request()
+		{
+			
+		}
+	
+
+}
+
+?>

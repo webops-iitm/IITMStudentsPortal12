0f26a5cc90c4a57f6de806e421c3aec06f19e216
ravikanthuppuluri <ravikanthuppuluri@gmail.com>
2012-11-02 17:57:47 +0530

    content of showprofile

---
+++ apps/students-search/show_profile.php
@@ -0,0 +1,69 @@
+<?php
+
+
+include("db.php");
+
+
+session_start();
+
+if (isset($_SESSION['uname']))
+
+  {
+  $query = "select * from users where upper(username) like \"%$_GET['user']%\"
+
+order by fullname";
+
+$numresults=mysql_query($query);
+
+$row = mysql_fetch_array($numresults);
+$name=$row['fullname'];
+$nick=$row['nick'];
+$user=$row['username'];
+$hostel=$row['hostel'];
+$room=$row['room'];
+$email=$row['email'];
+$contact=$row['contact'];
+
+
+  }
+
+
+
+?>
+
+
+
+
+
+
+<div class="widget-header">
+						<i class="icon-star"></i>
+						<h3><?php if($nick!="") echo $nick."'s"; ?>Profile</h3>
+					</div> <!-- /widget-header -->
+					
+					<div class="widget-content">
+	          			<table>
+							<tr>
+								<td style="width:100px;"><a href="#">Name</a></td>
+								<td><?php echo $name; ?></td>
+							</tr><tr>
+								<td style="width:100px;"><a href="#">Nickname</a></td>
+								<td><?php echo $nick; ?></td>
+							</tr><tr>
+								<td style="width:100px;"><a href="#">Roll Number</a></td>
+								<td><?php echo $user; ?></td>
+							</tr><tr>
+								<td style="width:100px;"><a href="#">Room No.</a></td>
+								<td><?php echo $room; ?></td>
+							</tr><tr>
+								<td style="width:100px;"><a href="#">Hostel.</a></td>
+								<td><?php echo $hostel; ?></td>
+							</tr><tr>
+								<td style="width:100px;"><a href="#">Contact No.</a></td>
+								<td><?php if($contact!=0) echo $contact; ?></td>
+							</tr><tr>
+								<td style="width:100px;"><a href="#">E-Mail ID</a></td>
+								<td><?php echo $email; ?></td>
+							</tr>
+						</table>
+				</div> <!-- /widget-content -->

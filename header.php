772825315314f7437dec7dcfd1e7a3d79f21a330
Vineet Upadhyay <vineet.1991.483@gmail.com>
2012-11-04 03:10:27 +0530

    mess operations

--- header.php
+++ header.php
@@ -80,15 +80,27 @@
 														<!--<li><a href="javascript:update('apps/caterer_rating/rating.php', 'widget');">Mess rating</a></li>-->
 														<!--<li><a href="javascript:;">forums</a></li>-->
 						</ul>
-				</li>			
+				</li>	
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+						Mess Operations
+						<b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="javascript:;">Mess Registration</a></li>
+							<li><a href="javascript:update('apps/caterer_rating/rating.php', 'widget');">Mess Rating</a></li>
+							<li><a href="javascript:;">Feedback</a></li>
+							<li><a href="javascript:;">New Extras Cards</a></li>
+							<li><a href="javascript:;">New Pin Request</a></li>
+						</ul>
+				</li>				
 				<li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
 						Activities
 						<b class="caret"></b></a>
 						<ul class="dropdown-menu">
-														<li><a href="javascript:;">techsoc</a></li>
-														<li><a href="javascript:;">litsoc</a></li>
-														<li><a href="javascript:;">schroeter</a></li>
+														<li><a href="http://www.shaastra.org/techsoc/" target="_blank" >TechSoc</a></li>
+														<li><a href="javascript:;">LitSoc</a></li>
+														<li><a href="http://www.sports.iitm.ac.in" target="_blank" >Schroeter</a></li>
 						</ul>
 				</li>
 				<li class="dropdown">
@@ -105,7 +117,7 @@
 						<b class="caret"></b></a>
 						<ul class="dropdown-menu">
 														<li><a href="javascript:update('apps/home/sac-meeting.php', 'widget');">SAC Meetings</a></li>
-														<li><a href="javascript:acupdate('apps/home/sac-members.php', 'widget');">SAC Members</a></li>
+														<li><a href="javascript:update('apps/home/sac-members.php', 'widget');">SAC Members</a></li>
 														<li><a href="files/sac-files/Constitution_revised.pdf" target="_blank">Revised constitution</a></li>
 														<li><a href="files/sac-files/Constitution_Amendments.pdf" target="_blank">Constitution Amendments</a></li>
 														<li><a href="javascript:;">Film club</a></li>

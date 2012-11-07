47687bbabaf0bc899f221d42958cd79c80f16878
vsvrp-1995 <vsvrp.1995@gmail.com>
2012-11-07 17:38:48 +0530

    sacmembers uupdate

--- header.php
+++ header.php
@@ -192,7 +192,7 @@
 						<b class="caret"></b></a>
 						<ul class="dropdown-menu">
 														<li><a href="javascript:update('apps/home/sac-meeting.php', 'widget');">SAC Meetings</a></li>
-														<li><a href="javascript:update('apps/home/sac-members.php', 'widget');">SAC Members</a></li>
+														<li><a href="javascript:update('apps/home/sac-membersnew.php', 'widget');">SAC Members</a></li>
 														<li><a href="files/sac-files/Constitution_revised.pdf" target="_blank">Revised constitution</a></li>
 														<li><a href="files/sac-files/Constitution_Amendments.pdf" target="_blank">Constitution Amendments</a></li>
 														<li><a href="javascript:update('apps/home/film-members.php','widget');">Film club</a></li>

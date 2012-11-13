4655a004e2d0dd4100a585baf4c9deb3b06ccf38
Abdeali J K <abdealikothari@gmail.com>
2012-11-12 20:14:43 -0800

    Bugs fixed in student search

--- apps/students-search/secretary/contact_sec.php
+++ apps/students-search/secretary/contact_sec.php
@@ -20,7 +20,10 @@
 <center>
 <form id="form" name="regform" action="apps/home/secretary/contactsubmit_sec.php" method="post">
 	<table style="float:left; margin-left:30px; margin-top:5px;">
-		<tr>
+    <tr>
+      <td colspan="2">
+<p>Got any Issues ? Write to the secretary !</p></td>
+    </tr><tr>
 			<td style="width:100px;"><a href="#">Nick</a></td>
 			<td>
         <input style="color:#fff;" id="nick" type="text" name="nick" value="<?php if( $nick == '' ) echo $name; else echo $nick; ?>" readonly='readonly' maxlength="255" />
@@ -32,10 +35,10 @@
 			<td><input style="color:#fff;" id="roll" type="text" name="roll" value="<?php echo $user; ?>" readonly='readonly' maxlength="8" /></td>
 		</tr><tr>
 			<td style="width:100px;"><a href="#">Contact No.</a></td>
-			<td><input id="contact" type="text" name="contact" value="<?php echo $contact; ?>" maxlength="10" onChange="if(this.value != 'admin') this.value = this.value.toUpperCase();"/></td>
+			<td><input id="contact" type="text" name="contact" value="<?php if($contact != 0) echo $contact; ?>" maxlength="10" onChange="if(this.value != 'admin') this.value = this.value.toUpperCase();"/></td>
 		</tr>
 		<tr>
-		<td  style="height:100px;"></td>
+		<td  style="height:80px;"></td>
 		</tr>
 		<tr>
 			<center>

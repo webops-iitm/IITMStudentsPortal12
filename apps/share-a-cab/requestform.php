5bada81824b5a85828c52116391f1b8e98520386
prasanthsai <g.prasanthsai@gmail.com>
2013-02-03 18:47:48 -0800

    share a cab intial commit

--- apps/share-a-cab/requestform.php
+++ apps/share-a-cab/requestform.php
@@ -13,11 +13,11 @@ if (!isset($_GET['rid']))
 	{
 		die("Some thing went wrong!");
 	}
-if (isset($_GET['numrequired']) && isset($_GET['requestby']))
+if (isset($_GET['comment']))
 	{
-		$result = $share->add_request($_GET['rid'], $_GET['numrequired'], $_GET['requestby'], $_GET['comment']);
+		$result = $share->add_request($_GET['rid'], $_GET['comment']);
 			if ($result)
-				die("Requset success");
+				die("Request success");
 	}
 $result = $share->select_cab($_GET['rid']);
 if($result)
@@ -37,31 +37,22 @@ if($result)
 
 <form class="form-horizontal" action="javascript:requestcab_url();">
 	<input type="hidden" id="rid" value="<?php echo $_GET['rid']; ?>">
-	<div class="control-group">
-		<label class="control-label" for="numrequired">Number of seats required</label>
-			<div class="controls">
-				<input type="text" id="numrequired" placeholder="">
-			</div>
-	</div>
-	<div class="control-group">
-		<label class="control-label" for="requestby">Requested by</label>
-			<div class="controls">
-				<input type="text" id="requestby" placeholder="">
-			</div>
-	</div>
+	
 	<div class="control-group">
 		<label class="control-label" for="comment">Comment</label>
 			<div class="controls">
-				<input type="textfield" id="comment" placeholder="">
+				<input type="text" id="comment" placeholder="">
 			</div>
 	</div>
+	
 	<div class="control-group">
 		<div class="controls">
 			<label class="checkbox">
 				<input type="checkbox"> Email notifications
 			</label>
-			<button type="submit" class="btn">Submit</button>
+			<button type="submit" value="requestseat" class="btn">Request a seat</button>
 		</div>
 	</div>
+	
 </form>
 

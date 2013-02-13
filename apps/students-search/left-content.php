e7967f026c963815907b49f83e66e931f99b939f
ravikanthuppuluri <ravikanthuppuluri@gmail.com>
2013-02-13 19:36:54 +0530

    adding instant sesrch feature and news search

--- apps/students-search/left-content.php
+++ apps/students-search/left-content.php
@@ -1,5 +1,60 @@
+<script src="js/jquery.js">
+
+</script>
+<script>
 
 
+var tim = 0 ;
+var pres = 0 ;
+
+var it = "students" ;
+
+
+function getdetails()
+{
+	tim = tim+1 ;
+	
+	$("#seres").css("opacity","0.4");
+	$("#loadinggif").css("display","inline");
+	
+	$.get("apps/students-search/fetda.php?q="+document.getElementById("q").value+"&item="+it+"&tim="+tim ,function(data) {
+		
+		
+		var k = eval('(' + data + ')');
+		
+		var numbcurr = parseInt(k.curr) ;
+		if ( numbcurr > pres)
+		{
+		document.getElementById("seres").innerHTML = k.cont ;
+		pres = numbcurr ;
+		if (tim == pres)
+		{
+			$("#seres").css("opacity","1");
+			$("#loadinggif").css("display","none");
+		}
+		}
+		}
+		);
+	
+}
+
+function setitem(val)
+{
+	if (!(it == val))
+	{
+	it = val ;
+	getdetails() ;
+	}
+	
+}
+
+
+
+
+
+
+
+</script>
 
 
 <div class="span8">
@@ -10,148 +65,20 @@
 		</div> <!-- /widget-header -->
 		<div class="widget-content">						
 			<div class="shortcuts">
-Enter 'Name' or 'Nick' or 'Roll number' or 'Room number' or 'Hostel' or 'Course' or 'Course Id' or 'Faculty'
+Enter 'Name' or 'Nick' or 'Roll number' or 'Room number' or 'Hostel' or 'Course' or 'Course Id' or 'Faculty' or 'News'
 <br/>	<br/>  
-<form name="form1" action="student-search.php" method="get">
-  <table><tr><td><input type="text"  name="q" /></td><td><input style="position:relative;top:-4px;"class="btn btn-primary" type="submit" name="Submit" value="Search" /></td></tr>
-  <tr><td><input type="radio" style="float:left" checked="checked" name="item" id="students" value="students"><label style="float:left" for ="students"> &nbsp; Students &nbsp; &nbsp;&nbsp;</label></input> <input type="radio" style="float:left" name="item" id="courses" value="courses"><label style="float:left" for ="courses"> &nbsp; Courses</label></input></td></tr>
+<form>
+  <table><tr><td><input type="text" id = "q" name="q" onkeyup = "getdetails()" /></td><td><div style="position:relative;top:-4px;"class="btn btn-primary"   onclick="getdetails()">Search </div></td></tr>
+  <tr><td><input type="radio" onclick = "setitem(this.value)" style="float:left" checked="checked" name="item" id="students" value="students"><label style="float:left" for ="students"> &nbsp; Students &nbsp; &nbsp;&nbsp;</label></input> <input type="radio" onclick = "setitem(this.value)" style="float:left" name="item" id="courses" value="courses"><label style="float:left" for ="courses"> &nbsp; Courses &nbsp; &nbsp;&nbsp;</label></input> <input type="radio" onclick = "setitem(this.value)" style="float:left"  name="item" id="news" value="news"><label style="float:left" for ="news"> &nbsp; News &nbsp; &nbsp;&nbsp;</label></input></td></tr>
   </table>
 </form>
 
 
+<div style="position:relative" ><img src="loading.gif" id="loadinggif" style = "position:relative;display:none; left:50%" /><div id = "seres"  >  </div></div>
 
 
 
 
-<?php
-
-session_start();
-
-if (isset($_SESSION['uname']))
-
-  {
- if (isset($_GET['q']))
-  {
-
-
-  // Get the search variable from URL
-
-  $var = @$_GET['q'] ;
-  
-  
-  $var = strtoupper($var); //converting to uppercase
-  $trimmed = trim($var) ; //trim whitespace from the stored variable
-  
-  
-  
-
-
-  
-  
-  
-  
-  // check for an empty string and display a message.
-if ($trimmed == "")
-  {
-  echo "<p style='color:red;'>Please enter a search hint...</p>";
-  exit;
-  }
-  
-
-
-
-
-if ( $_GET['item'] == "students" )
-{
- $query = "select * from users where upper(fullname) like \"%$trimmed%\" OR upper(nick) like \"%$trimmed%\" OR upper(username) like \"%$trimmed%\" OR upper(hostel) like \"%$trimmed%\" OR upper(room) like \"%$trimmed%\" 
-order by fullname"; 
-}
-
-if ( $_GET['item'] == "courses" )
-{
- $query = "select * from courses where upper(name) like \"%$trimmed%\" OR upper(number) like \"%$trimmed%\" OR upper(prof) like \"%$trimmed%\" 
-order by name"; 
-}
-
-
-  $numresults=mysql_query($query);
- $numrows=mysql_num_rows($numresults);
- echo "<h4 style='text-shadow:none;' >Search results</h4>";
- if ($numrows == 0)
-  {
-  
-  echo "<p style='color:red;'>Sorry, your search &quot;" . $trimmed . "&quot; returned no results</p>";
-  exit;
-  }
-  
-  
-  if ( $_GET['item'] == "students" )
-  {
-  echo "<table border='1' cellpadding='5' style='background:white;border:10px solid #E9E9E9 ;' align='center' >
-<tr>
-<th>Name</th>
-<th>Nick</th>
-<th>Roll No</th>
-<th>Hostel</th>
-<th>Room No</th>
-</tr>";
-
-while($row = mysql_fetch_array($numresults))
-  {
-  echo "<tr>";
-  echo "<td> <a href='student-search.php?userd=".$row['username']."' >"  . $row['fullname'] . "</a> </td>";
-  echo "<td>" . $row['nick'] . "</td>";
-  echo "<td>" . $row['username'] . "</td>";
-  echo "<td>" . $row['hostel'] . "</td>";
-  echo "<td>" . $row['room'] . "</td>";
-  echo "</tr>";
-  }
-echo "</table>";
-}
-
-if ( $_GET['item'] == "courses" )
-{
-  echo "<table border='1' cellpadding='5' style='background:white;border:10px solid #E9E9E9 ;' align='center' >
-<tr>
-<th>Course name</th>
-<th>Course ID</th>
-<th>Faculty</th>
-
-</tr>";
-
-while($row = mysql_fetch_array($numresults))
-  {
-  echo "<tr>";
-  echo "<td>" . $row['name'] . "</td>";
-  echo "<td>" . $row['number'] . "</td>";
-  echo "<td>" . $row['prof'] . "</td>";
-  
-  echo "</tr>";
-  }
-echo "</table>";
-
-
-
-}
-mysql_close($con);
-
-
-
-}
-
-
-include("db.php");
-
-
-
-
-
-
- }
-   else {echo "<p style='color:red;'>Please login to search</p>";}
-
-
-?>
 <br/><br/>
 			</div>	
 		</div>

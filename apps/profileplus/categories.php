94edbf8fc5e493943e987ddf4f1afecbfce437ad
vsvrp-1995 <vsvrp.1995@gmail.com>
2013-03-10 03:53:11 +0530

    education resume update

--- apps/profileplus/categories.php
+++ apps/profileplus/categories.php
@@ -1,33 +1,22 @@
-<div class="accordion-group">
-	<div class="accordion-heading">  
-    	<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordioncat" href="#collapse<?php echo $catID.$catname; ?>">  
-                  <?php echo $catname; ?>
+    	<a><?php echo $catname; ?>
                 </a>  
-    </div>  
-    <div id="collapse<?php echo $catID.$catname; ?>" class="accordion-body collapse" style="height: 0px; ">  
-    	<div class="accordion-inner">
                   <p>Sections under which you want it on the Profile Page : </p>
-                  <div class="accordion" id="accordion<?php echo $catname; ?>">
-                  <?php
-				  $subcatresult = mysql_query("SELECT * FROM $SubCatTable where cat_id = $catID");
-				   while($subcatrow = mysql_fetch_array($subcatresult)){
-					   $subcatname = $subcatrow['name'];
-					   $subcatID = $subcatrow['id'];
-					   $subcatparent = $subcatrow['cat_id'];
-					   $subcatdesc = $subcatrow['description'];
-					   $subcattype = $subcatrow['type'];
-					   
-					    include 'subcategories.php'; 
-				   }
-						?>
                   <form class="form-horizontal">  
  						<fieldset>
+                        <div class="control-group">  
+            			        <label class="control-label" for="Qualihesc">Heading</label>  
+            				<div class="controls">  
+              					<input type="text" class="input-xlarge" name="qualihead" id="QualiHead">   
+            				</div> 
+          				</div>
+                        <div class="control-group">  
+            			        <label class="control-label" for="Qualidesc">Description</label>  
+            				<div class="controls">  
+              					<input type="text" class="input-xlarge" name="qualidesc" id="QualiDesc">   
+            				</div> 
+          				</div>
         	                        <div class="form-actions">  
             <button type="submit" class="btn btn-primary">Save changes</button>  
             <button class="btn">Cancel</button>  
           							</div> 
    						</fieldset></form>
-                        </div>
-		</div>  
-	</div>
-</div>

3a27deb11c4fb38925ba5ae0cd05248f96182573
Yash Murty <yashmurty@gmail.com>
2013-07-17 23:11:33 +0530

    IITM Logo and Landing Page

--- landing/metro/index.php
+++ landing/metro/index.php
@@ -13,7 +13,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"><!--NoM4D--><head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Metro Landing</title>
+<title>Landing Page</title>
+<link rel="shortcut icon" href="images/IITM_Color_Logo_30px.png" type="image/png">
 
 <link href="css/modern.css" rel="stylesheet">
 
